b1763bc12ae65ea5f337659f34505e64dd4d54d7
Kevin Burke <kevinhenryburke@yahoo.co.uk>
2018-09-09 11:15:42 +0100

    New component bzChartHierarchy due to corruption(?) of bzHierarchy?

---
+++ connectors/force-app/main/default/aura/bzChartHierarchy/bzChartHierarchyController.js
@@ -0,0 +1,32 @@
+({
+
+    /* receive a bubbled component event and distribute this to required children */
+
+    handle_evt_sfd3  : function(component, event, helper) {
+        bzutils.log('bzHierarchy: handle_evt_sfd3 enter');
+
+        var topic = event.getParam("topic");
+        var parameters = event.getParam("parameters");
+        var controller = event.getParam("controller");
+
+        var tpc = {
+            "topic" : topic,
+            "parameters" : parameters,
+            "controller" : controller,
+        };
+
+        var chartHierarchy = component.find("chartHierarchy");
+        chartHierarchy.callFromContainer(tpc);
+
+        var d3comp = component.find("d3comp");
+        d3comp.callFromContainer(tpc);
+
+        var showTopPanel = component.get("v.showTopPanel");
+        if (showTopPanel == true) {
+            var panelDisplay = component.find("panelDisplay");
+            panelDisplay.callFromContainer(tpc);
+        }
+
+
+    },
+})
