50db3445319aec2e5bcdf53c2c47484db6c87cfc
Kevin Burke <kevinhenryburke@yahoo.co.uk>
2018-11-04 23:00:46 +0000

    Rename of classes to correct name convention

--- connectors/.sfdx/typings/lwc/apex/DataExtract.d.ts
+++ connectors/.sfdx/typings/lwc/apex/DataExtract.d.ts
@@ -1,15 +1,15 @@
-declare module "@salesforce/apex/DThree.returnNetworkInitial" {
+declare module "@salesforce/apex/DataExtract.returnNetworkInitial" {
   export default function returnNetworkInitial(param: {queryJSON: any, recordId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DThree.returnNetworkUpdate" {
+declare module "@salesforce/apex/DataExtract.returnNetworkUpdate" {
   export default function returnNetworkUpdate(param: {queryJSON: any, queryLevelIds: any, thisLevel: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DThree.returnUnlinkedData" {
+declare module "@salesforce/apex/DataExtract.returnUnlinkedData" {
   export default function returnUnlinkedData(param: {queryJSON: any, recordId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DThree.returnHierarchyTop" {
+declare module "@salesforce/apex/DataExtract.returnHierarchyTop" {
   export default function returnHierarchyTop(param: {queryJSON: any, recordId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/DThree.returnHierarchyLevels" {
+declare module "@salesforce/apex/DataExtract.returnHierarchyLevels" {
   export default function returnHierarchyLevels(param: {queryJSON: any, queryLevelIds: any, queryLevel: any}): Promise<any>;
 }
