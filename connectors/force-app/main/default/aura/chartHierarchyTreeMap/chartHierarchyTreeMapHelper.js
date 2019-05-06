9ecd480284b310802f4f2ba84804ef6aed0e4988
Kevin Burke <kevinhenryburke@yahoo.co.uk>
2019-05-06 21:56:50 +0100

    initializeVisuals more refactor

--- connectors/force-app/main/default/aura/chartHierarchyTreeMap/chartHierarchyTreeMapHelper.js
+++ connectors/force-app/main/default/aura/chartHierarchyTreeMap/chartHierarchyTreeMapHelper.js
@@ -1,8 +1,3 @@
 ({
-    initializeVisuals: function (component) {
-        let storeObject = component.get("v.storeObject");
-        let variantsMixin = bzchart.getStore (storeObject, "chartMixin") ;
-        variantsMixin.initializeVisuals(storeObject);
-    }
 
 })
